16ba6084cbe874ac7bf4e5423a8ff964f49cb6a5
Kevin Eddye <kevneddy@gmail.com>
2016-08-30 18:22:51 -0700

    Add corretion to webbrowser argument

--- movies.py
+++ movies.py
@@ -11,6 +11,6 @@ class = Movie():
 		self.poster.img_url = poster_img
 		self.trailer_youtube_url = trailer_youtube
 
-	# plays movie trailer in browser
+	# plays youtube movie trailer in browser
 	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
+		webbrowser.open(self.trailer_youtube)
